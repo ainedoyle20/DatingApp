e8274a5ee601b7480610f5da3478289f5650743d
Aine Doyle <aine@happen.software>
2023-07-05 14:50:44 +0100

    Section 4. Authorization, validation, Jwt tokens

--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,21 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+// Need to add the authentication middleware after UseCors and before MapControllers method
+app.UseAuthentication(); // 1st. Asks do you have a valid token
+app.UseAuthorization(); // 2nd. So you have a valid token, now what are you allowed to do?
 
 app.MapControllers(); // middleware that tells our http request which API endpoint it needs to go to
 
