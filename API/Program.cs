119e9f3e3d825da6862b4b0f4a9d1741cc1d85f2
Aine Doyle <aine@happen.software>
2023-07-07 12:43:10 +0100

    S8 - Extending the API

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,5 +23,19 @@ app.UseAuthentication(); // 1st. Asks do you have a valid token
 app.UseAuthorization(); // 2nd. So you have a valid token, now what are you allowed to do?
 
 app.MapControllers(); // middleware that tells our http request which API endpoint it needs to go to
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+  var context = services.GetRequiredService<DataContext>();
+  await context.Database.MigrateAsync();
+  await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+  var logger = services.GetService<ILogger<Program>>();
+  logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run(); // command to actually run the application
